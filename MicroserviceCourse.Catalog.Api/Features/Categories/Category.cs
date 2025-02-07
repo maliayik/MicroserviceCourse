16b6d97a7c21299b1246739c93d7fbbd6a9c806b
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-02-07 15:26:34 +0300

    Course/Category Entity added

---
+++ MicroserviceCourse.Catalog.Api/Features/Categories/Category.cs
@@ -0,0 +1,11 @@
+﻿using MicroserviceCourse.Catalog.Api.Repositories;
+
+namespace MicroserviceCourse.Catalog.Api.Features.Categories
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = default!;
+        public List<Course>? Courses { get; set; }
+
+    }
+}
---
+++ MicroserviceCourse.Catalog.Api/Features/Categories/Course.cs
@@ -0,0 +1,17 @@
+﻿using MicroserviceCourse.Catalog.Api.Repositories;
+
+namespace MicroserviceCourse.Catalog.Api.Features.Categories
+{
+    public class Course : BaseEntity
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public decimal Price { get; set; }
+        public Guid UserId { get; set; }
+        public string? Picture { get; set; }
+        public DateTime Created { get; set; }
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; } = default!;
+
+    }
+}
