41354f9e2e5ca140386109d6bcac99c677edd8fb
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-02-11 11:03:14 +0300

    AppDbContext added

---
+++ MicroserviceCourse.Catalog.Api/Repositories/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using MicroserviceCourse.Catalog.Api.Features.Categories;
+using MicroserviceCourse.Catalog.Api.Features.Courses;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace MicroserviceCourse.Catalog.Api.Repositories
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //configure yaptığımız entitylerin çalıştığımız assembly içerisinde olduğunu belirtiyoruz
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
