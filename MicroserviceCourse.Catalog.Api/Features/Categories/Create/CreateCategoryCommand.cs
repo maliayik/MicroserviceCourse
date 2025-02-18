a2a9c2776c3c4b50a11c5d4cfa3b85b8079abb1f
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-02-18 23:03:13 +0300

    MediatR libary initialize , CreateCategoryCommandHandler added

---
+++ MicroserviceCourse.Catalog.Api/Features/Categories/Create/CreateCategoryCommand.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.Runtime.Internal;
+using MediatR;
+using MicroserviceCourse.Shared;
+
+namespace MicroserviceCourse.Catalog.Api.Features.Categories.Create
+{
+    //Command ile bitenler Create, update, insert , delete işlemleri için kullanılır.
+    //record olarak tanımladık, çünkü dışarıdan immutability (değiştirilemez) olmasını istiyoruz.
+
+    public record CreateCategoryCommand(string Name) : IRequest<ServiceResult<CreateCategoryResponse>>;
+
+}
