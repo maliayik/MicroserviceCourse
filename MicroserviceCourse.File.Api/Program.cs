a80776482524351e7597d17c5c8339486060a750
maliiayik <maliiayik@gmail.com>
2025-03-23 13:15:00 +0300

    section/file added

---
+++ MicroserviceCourse.File.Api/Program.cs
@@ -0,0 +1,25 @@
+using MicroserviceCourse.File.Api;
+using MicroserviceCourse.Shared.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCommonServiceExt(typeof(FileAssembly));
+builder.Services.AddVersioingExt();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapOpenApi();
+}
+
+app.Run();
+
