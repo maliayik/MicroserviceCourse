5a435273ddbf8e85e42db15c31bc2c229140e347
maliiayik <maliiayik@gmail.com>
2025-04-12 21:29:11 +0300

    CreateOrderCommand added

---
+++ MicroserviceCourse.Order.Application/Features/Orders/Create/CreateOrderCommand.cs
@@ -0,0 +1,11 @@
+using MicroserviceCourse.Order.Domain.Entities;
+
+namespace MicroserviceCourse.Order.Application.Features.Orders.Create;
+
+public record CreateOrderCommand(float? DiscountRate, AddressDto Address, PaymentDto Payment, List<OrderItemDto> Items);
+
+public record AddressDto(string Province, string Street, string District, string ZipCode, string Line);
+
+public record PaymentDto(string CardNumber, string CardHolderName, string Expiration, string Cvc, decimal Amount);
+
+public record OrderItemDto(Guid ProductId, string ProductName, decimal UnitPrice);
