ff41a3febad83c1601bb9cb3daa335a7f88df40b
maliiayik <maliiayik@gmail.com>
2025-04-19 14:12:31 +0300

    GetOrdersByBuyerIdAsync added

--- MicroserviceCourse.Order.Application/Contracts/Repositories/IOrderRepository.cs
+++ MicroserviceCourse.Order.Application/Contracts/Repositories/IOrderRepository.cs
@@ -2,4 +2,5 @@ namespace MicroserviceCourse.Order.Application.Contracts.Repositories;
 
 public interface IOrderRepository : IGenericRepository<Guid, Domain.Entities.Order>
 {
+    public Task<List<Domain.Entities.Order>> GetOrdersByBuyerIdAsync(Guid buyerId);
 }
--- MicroserviceCourse.Order.Persistence/Repositories/OrderRepository.cs
+++ MicroserviceCourse.Order.Persistence/Repositories/OrderRepository.cs
@@ -1,8 +1,15 @@
 using MicroserviceCourse.Order.Application.Contracts.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace MicroserviceCourse.Order.Persistence.Repositories;
 
 public class OrderRepository(AppDbContext context)
     : GenericRepository<Guid, Domain.Entities.Order>(context), IOrderRepository
 {
+    public Task<List<Domain.Entities.Order>> GetOrdersByBuyerIdAsync(Guid buyerId)
+    {
+        return context.Orders.Include(x => x.OrderItems).Where(x => x.BuyerId == buyerId)
+            .OrderByDescending(x => x.Created)
+            .ToListAsync();
+    }
 }
